ace185ea222c9dfdbff17ad5d97bb8797d30bcd7
buyuanlee <buyuanlee@gmail.com>
2018-11-26 15:32:25 +0800

    父子组件之间相互传递数据

---
+++ src/components/parent.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <span>我是父组件</span>---{{fromSonMsg}}
+    <hr>
+    <son v-bind:msg="toSonMsg" @handle="getMsgFromSon"></son>
+  </div>
+</template>
+
+<script>
+  import son from './son'
+
+  export default {
+    name: 'parent',
+    components: {
+      son
+    },
+    data: function () {
+      return {
+        toSonMsg: '我是你的父亲',
+        fromSonMsg: ''
+      }
+    },
+    methods: {
+      getMsgFromSon: function (value) {
+        this.fromSonMsg = value
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/son.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <span>我是子组件</span>------{{msg}}
+    <button @click="sendMsgToFather">传递数据至父组件</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'son',
+    data: function () {
+      return {
+        toFartherMsg: '我是你的儿子'
+      }
+    },
+    props: {
+      msg: {
+        type: String,
+        default: ''
+      }
+    },
+    methods: {
+      sendMsgToFather: function () {
+        this.$emit('handle', this.toFartherMsg)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
